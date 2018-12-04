6a6d930a2937cc1a3355053d8e2656e9007b960b
Maria Rosa Rosete <ross.zhena@gmail.com>
2018-12-04 13:14:55 -0600

    Se agrega funcionalidad de envio Tweet

--- src/js/main.js
+++ src/js/main.js
@@ -9,30 +9,33 @@ renderInfo = (data) => {
             const re = /.{3}-.{7}-.{7}/g;
             const searchT = message.match(re)
                 //console.log(searchT);
-                // console.log(message);
+                //console.log(message);
 
             if (searchT != null) {
                 result += `<div class="card">
                              <div class="info">
                              <p>Id: ${data[prop].id}</p> 
+                             <p>Name: ${data[prop].user.name}</p>
                              <img src="${data[prop].user.biggerProfileImageURL}" alt="Smiley face" height="42" width="42">
-                                <p>Name: ${data[prop].user.name}</p> 
+                                <p>Name: ${data[prop].user.name}</p>
+                                <p>Name: ${data[prop].user.screenName}</p> 
                                 <p>Twitte: ${data[prop].text}</p>
                                 <p>Fecha: ${data[prop].createdAd}</p>
                                 <input type="text" id="myTextResponse" value="Some text...">
-                                <a class='btn-delete' data-message="${data[prop].id}">Try it</a>
+                                <a class='btn-send' data-message="${data[prop].id}, ">Try it</a>
                              </div>
                          </div>`
             }
             container.innerHTML = result;
             if (container != '') {
-                let elementsDelete = document.getElementsByClassName('btn-delete');
-                for (let i = 0; i < elementsDelete.length; i++) {
-                    //console.log(elementsDelete[i]);
-                    elementsDelete[i].addEventListener('click', e => {
+                let elementsTweets = document.getElementsByClassName('btn-send');
+                for (let i = 0; i < elementsTweets.length; i++) {
+                    // console.log(elementsTweets[i]);
+                    elementsTweets[i].addEventListener('click', e => {
                         let key = e.target;
                         let keyDataDelete = key.getAttribute("data-message");
                         console.log(keyDataDelete)
+                        tweetResponse();
                     });
                 };
 
@@ -43,6 +46,18 @@ renderInfo = (data) => {
 
 tweetResponse = () => {
     console.log("funcion tweetResponse")
+    var url = 'https://wmt-laboratoria.herokuapp.com/tweets';
+    var data = "En seguimiento a tu pedido"
+
+    fetch(url, {
+            method: 'POST',
+            body: data, // data can be `string` or {object}!
+            headers: {
+                'Content-Type': 'text/plain'
+            }
+        }).then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(response => console.log('Success:', response));
 
 }
 
